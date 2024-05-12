ceec668937fe91852b3bf8af9d2ecadab2b3a9e4
amandi <IT22894892@my.sliit.lk>
2024-05-12 19:53:43 +0530

    added the sql and database parts

--- app/src/main/java/com/example/todo/AddToDoActivity.kt
+++ app/src/main/java/com/example/todo/AddToDoActivity.kt
@@ -1,20 +1,31 @@
 package com.example.todo
 
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.todo.databinding.ActivityAddToDoBinding
+import com.example.todo.databinding.ActivityAddToDoBinding
 
 class AddToDoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddToDoBinding
+    private lateinit var db:TodoDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_to_do)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAddToDoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TodoDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val todo = Todo(0,title,content)
+            db.insertTodo(todo)
+            finish()
+            Toast.makeText(this,"Todo saved",Toast.LENGTH_SHORT).show()
         }
     }
 }
